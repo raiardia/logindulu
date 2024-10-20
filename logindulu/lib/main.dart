64f6eb3e1e38ed3f1115ccbea7c6c2c09605becb
raiardia <ardiaanggreni@gmail.com>
2024-10-20 16:47:10 +0800

    Menambahkan MyApp

--- logindulu/lib/main.dart
+++ logindulu/lib/main.dart
@@ -4,3 +4,12 @@ void main() {
   runApp(MyApp());
 }
 
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: LoginPage(),
+    );
+  }
+}
